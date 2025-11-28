3f7286aa18a3c7385b0182b936d1790fdfd62e7f
Iqbal Guntur <iqbalguntur313@gmail.com>
2025-11-28 08:27:50 +0700

    menambahkan views dan urls

--- urls.py
+++ urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adminpanel/', include('adminpanel.urls')),  # tambahkan ini
+    path('chat/', views.chat_view, name='chat'),
 ]
 
